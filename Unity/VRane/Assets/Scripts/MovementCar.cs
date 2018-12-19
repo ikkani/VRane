c09e34a3973539d62fc41e3e98795fafd29c539e
ikkani <urrutisanchez@hotmail.com>
2018-12-19 19:14:52 +0100

    update

---
+++ Unity/VRane/Assets/Scripts/MovementCar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementCar : MonoBehaviour {
+
+    public float velCar;
+    int sense;
+    public Transform craneBoom;
+    Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        sense = (int)Input.GetAxisRaw("Vertical");
+        transform.localPosition = new Vector3(0, transform.localPosition.y, transform.localPosition.z + (sense * velCar * Time.deltaTime));
+        transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+
+    }
+
+    private void FixedUpdate()
+    {
+    }
+}
---
+++ Unity/VRane/Assets/Scripts/MovementCraneBoom.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementCraneBoom : MonoBehaviour {
+
+    public float velRotation;
+    private int rotCraneBoom;
+    private Rigidbody rb;
+    // Use this for initialization
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        rotCraneBoom = (int)Input.GetAxisRaw("Horizontal");
+    }
+
+    private void LateUpdate()
+    {
+        if (rotCraneBoom != 0)
+            rb.AddTorque(transform.up * (rotCraneBoom * velRotation));
+
+    }
+
+
+
+   }
